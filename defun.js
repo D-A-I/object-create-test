9b7bfb6606b7f9ba571cb29f927064ec573f24e6
D--A--I <kanegadai@kanegadainoMacBook-Pro.local>
2019-04-30 18:22:26 +0900

    初回コミット

---
+++ defun.js
@@ -0,0 +1,47 @@
+/* ファクトリメソッド用のオブジェクト
+ * - Animal - */
+const Animal = {
+    create(type) {
+        let animal = Object.create(Animal.prototype);
+        animal.type = type;
+        return animal;
+    },
+    isAnimal(obj, type) {
+        // objのプロトタイプチェーン上にAnimalが存在するか
+        if (!Animal.prototype.isPrototypeOf(obj)) {
+            return false;
+        }
+        return type ? obj.type === type : true;
+    },
+    prototype: {}
+};
+/* ファクトリメソッド用のオブジェクト
+ * - Cat - */
+const Cat = {
+    create(name, bread) {
+        let cat = Object.create(Cat.prototype);
+        // Animalのパブリックプロパティ付与
+        Object.assign(cat, Animal.create('Cat'));
+        // メンバ変数
+        cat.name = name;
+        cat.bread = bread;
+        return cat;
+    },
+    isCat(obj) {
+        return Animal.isAnimal(obj, 'Cat');
+    },
+    prototype: {
+        sing() {
+            console.log('nya nya nya');
+        },
+        introduce() {
+            console.log(`My name is '${this.name}' The '${this.bread}'.`);
+        }
+    }
+};
+
+// "prototypeのprototype"を設定
+Object.setPrototypeOf(Cat.prototype, Animal.prototype);
+
+// 外部に公開する
+module.exports = Cat;
---
+++ index.js
@@ -0,0 +1,9 @@
+const Cat = require('./defun');
+
+const kurumi = Cat.create('くるみ', 'ノルウェージャンフォレストキャット');
+kurumi.introduce();
+kurumi.sing();
+
+const mellow = Cat.create('メロウ', 'ボンベイ');
+mellow.introduce();
+mellow.sing();
